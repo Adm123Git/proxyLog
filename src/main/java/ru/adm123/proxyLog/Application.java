0da505e2d2d2f514bf6a7d6bcfe1d8d1cc172fb3
Dmitry_Ushakov <Dmitry_Ushakov@russianpost.ru>
2022-01-19 11:00:24 +0300

    add Application class

---
+++ src/main/java/ru/adm123/proxyLog/Application.java
@@ -0,0 +1,10 @@
+package ru.adm123.proxyLog;
+
+public class Application {
+
+	public static void main(String[] args) {
+		TestLogging testLogging = new TestLogging();
+		testLogging.calculation(5);
+	}
+
+}
--- src/main/java/ru/adm123/proxyLog/TestLogging.java
+++ src/main/java/ru/adm123/proxyLog/TestLogging.java
@@ -6,6 +6,7 @@ public class TestLogging {
 
 	@Log
 	public void calculation(int param) {
+		System.out.println("calculating...");
 	}
 
 }
