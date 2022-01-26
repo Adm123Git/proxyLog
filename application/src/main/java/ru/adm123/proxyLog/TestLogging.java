8b1b7b6c3f71912a20a5fedbf5434f35b9941a4a
Adm123 <adm123@mail.ru>
2022-01-26 12:36:55 +0300

    anotation parameters to test class

--- application/src/main/java/ru/adm123/proxyLog/TestLogging.java
+++ application/src/main/java/ru/adm123/proxyLog/TestLogging.java
@@ -4,7 +4,7 @@ import ru.adm123.logAgent.annotation.LogExecuteTime;
 
 public class TestLogging {
 
-	@LogExecuteTime
+	@LogExecuteTime(unit = LogExecuteTime.TimeUnit.MILLIS, template = "System.out.println(\"'$methodName' executed at $executeTime milliSecs\");")
 	public void calculation(int param) throws InterruptedException {
 		System.out.println("calculating...");
 		Thread.sleep((long) (Math.random() * 100));
